aa5d87667bf28dd7c92eedf4aed42fe72cbf699b
Grigory Malivenko <nerox8664@gmail.com>
2019-06-25 20:36:14 +0300

    Refactor convolution layers.

--- onnx2keras/convolution_layers.py
+++ onnx2keras/convolution_layers.py
@@ -1,46 +1,51 @@
 import keras.layers
-import numpy as np
-import random
-import string
+import logging
 import tensorflow as tf
-from .common import random_string
-
-def extract_input_weight_bias(node, weights):
-    W = None
-    bias = None
-    input_name = None
-    for node_input in node.input:
-        if node_input.endswith('weight'):
-            W = weights[node_input]
-        elif node_input.endswith('bias'):
-            bias = weights[node_input]
-        else:
-            input_name = node_input
-    return input_name, W, bias, bias is not None
+from .utils import is_numpy
 
 
-def convert_conv(node, params, layers, weights, node_name):
+def convert_conv(node, params, layers, node_name):
     """
-    Convert convolution layer.
-
-    Args:
-        params: dictionary with layer parameters
-        w_name: name prefix in state_dict
-        scope_name: pytorch scope name
-        inputs: pytorch node inputs
-        layers: dictionary with keras tensors
-        weights: pytorch state_dict
-        names: use short names for keras layers
+    Convert convolution layer
+    :param node: current operation node
+    :param params: operation attributes
+    :param layers: available keras layers
+    :param node_name: resulting layer name
+    :return: None
     """
-    print('Converting convolution ...')
+    logger = logging.getLogger('onnx2keras:conv')
 
-    input_name, W, bias, has_bias = extract_input_weight_bias(node, weights)
+    if len(node.input) == 3:
+        logger.debug('Conv with bias')
+        # Has bias
+        has_bias = True
+        W = layers[node.input[1]]
+        bias = layers[node.input[2]]
+
+        if not is_numpy(W) or not is_numpy(bias):
+            raise AttributeError('Weights or bias is not a numpy array.')
+
+    elif len(node.input) == 2:
+        logger.debug('Conv without bias')
+        has_bias = False
+        W = layers[node.input[1]]
+        bias = None
+        if not is_numpy(W):
+            raise AttributeError('Weights or bias is not a numpy array.')
+
+    else:
+        raise NotImplementedError('Not implemented')
+
+    input_name = node.input[0]
 
     if len(W.shape) == 5:  # 3D conv
         raise NotImplementedError('Not implemented')
 
     elif len(W.shape) == 4:  # 2D conv
+        logger.debug('2D convolution')
+
         if params['pads'][0] > 0 or params['pads'][1] > 0:
+            logger.debug('Paddings exist, add ZeroPadding layer')
             padding_name = node_name + '_pad'
             padding_layer = keras.layers.ZeroPadding2D(
                 padding=(params['pads'][0], params['pads'][1]),
@@ -55,6 +60,7 @@ def convert_conv(node, params, layers, weights, node_name):
         in_channels = channels_per_group * n_groups
 
         if n_groups == in_channels and n_groups != 1:
+            logger.debug('Number of groups is equal to input channels, use DepthWise convolution')
             W = W.transpose(0, 1, 3, 2)
             if has_bias:
                 weights = [W, bias]
@@ -76,6 +82,8 @@ def convert_conv(node, params, layers, weights, node_name):
             layers[node_name] = conv(layers[input_name])
 
         elif n_groups != 1:
+            logger.debug('Number of groups more than 1, but less than number of in_channel, use group convolution')
+
             # Example from https://kratzert.github.io/2017/02/24/finetuning-alexnet-with-tensorflow.html
             def target_layer(x, groups=params['group'], stride_y=params['strides'][0], stride_x=params['strides'][1]):
                 x = tf.transpose(x, [0, 2, 3, 1])
@@ -119,24 +127,44 @@ def convert_conv(node, params, layers, weights, node_name):
         raise NotImplementedError('Not implemented')
 
 
-def convert_convtranspose(node, params, layers, weights, node_name):
+def convert_convtranspose(node, params, layers, node_name):
     """
-    Convert transposed convolution layer.
-
-    Args:
-        params: dictionary with layer parameters
-        w_name: name prefix in state_dict
-        scope_name: pytorch scope name
-        inputs: pytorch node inputs
-        layers: dictionary with keras tensors
-        weights: pytorch state_dict
-        names: use short names for keras layers
+    Convert transposed convolution layer
+    :param node: current operation node
+    :param params: operation attributes
+    :param layers: available keras layers
+    :param node_name: resulting layer name
+    :return: None
     """
-    print('Converting transposed convolution ...')
+    logger = logging.getLogger('onnx2keras:convtranpose')
 
-    input_name, W, bias, has_bias = extract_input_weight_bias(node, weights)
+    if len(node.input) == 3:
+        logger.debug('ConvTranspose with bias')
+        # Has bias
+        has_bias = True
+        W = layers[node.input[1]]
+        bias = layers[node.input[2]]
 
-    if len(W.shape) == 4:
+        if not is_numpy(W) or not is_numpy(bias):
+            raise AttributeError('Weights or bias is not a numpy array.')
+
+    elif len(node.input) == 2:
+        logger.debug('ConvTranspose without bias')
+        has_bias = False
+        W = layers[node.input[1]]
+        bias = None
+        if not is_numpy(W):
+            raise AttributeError('Weights or bias is not a numpy array.')
+
+    else:
+        raise NotImplementedError('Not implemented')
+
+    input_name = node.input[0]
+
+    if len(W.shape) == 5:  # 3D conv
+        raise NotImplementedError('Not implemented')
+
+    elif len(W.shape) == 4:  # 2D conv
         W = W.transpose(2, 3, 1, 0)
         height, width, n_filters, channels = W.shape
 
@@ -146,11 +174,11 @@ def convert_convtranspose(node, params, layers, weights, node_name):
             weights = [W]
                 
         n_groups = params['group']
-        if n_groups > 1:
-            raise AssertionError('Cannot convert conv1d with groups != 1')
+        if params['group'] > 1:
+            raise AttributeError('Cannot convert ConvTranspose2d with groups != 1')
 
         if params['dilations'][0] > 1:
-            raise AssertionError('Cannot convert conv1d with dilation_rate != 1')
+            raise AttributeError('Cannot convert ConvTranspose2d with dilation_rate != 1')
 
         conv = keras.layers.Conv2DTranspose(
             filters=n_filters,
@@ -172,8 +200,12 @@ def convert_convtranspose(node, params, layers, weights, node_name):
         # See the Keras issue: https://github.com/keras-team/keras/issues/6777
         layers[node_name].set_shape(layers[node_name]._keras_shape)
 
+        if 'output_padding' in params and (params['output_padding'][0] > 0 or params['output_padding'][1] > 0):
+            raise AttributeError('Cannot convert ConvTranspose2d with output_padding != 0')
+
         pads = params['pads']
         if pads[0] > 0:
+            logger.debug('Add cropping layer for output padding')
             assert(len(pads) == 2 or (pads[2] == pads[0] and pads[3] == pads[1]))
 
             crop = keras.layers.Cropping2D(
@@ -181,5 +213,7 @@ def convert_convtranspose(node, params, layers, weights, node_name):
                 name=node_name + '_crop'
             )
             layers[node_name] = crop(layers[node_name])
+
+
     else:
-        raise AssertionError('Layer is not supported for now')
+        raise AttributeError('Layer is not supported for now')
