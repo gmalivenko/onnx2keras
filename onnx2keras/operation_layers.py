c35252bdd58c35c6632888f4c9d864e087c61959
Grigory Malivenko <nerox8664@gmail.com>
2019-06-27 00:46:08 +0300

    Add reduce_mean layer.

--- onnx2keras/operation_layers.py
+++ onnx2keras/operation_layers.py
@@ -96,3 +96,27 @@ def convert_reduce_sum(node, params, layers, node_name):
 
     lambda_layer = keras.layers.Lambda(target_layer, name=node_name)
     layers[node_name] = lambda_layer(input_0)
+    layers[node_name].set_shape(layers[node_name]._keras_shape)
+
+
+def convert_reduce_mean(node, params, layers, node_name):
+    """
+    Convert reduce mean.
+    :param node: current operation node
+    :param params: operation attributes
+    :param layers: available keras layers
+    :param node_name: resulting layer name
+    :return: None
+    """
+    if len(node.input) != 1:
+        assert AttributeError('More than 1 input for reduce mean layer.')
+
+    input_0 = ensure_tf_type(layers[node.input[0]])
+
+    def target_layer(x, axis=params['axes'], keepdims=params['keepdims']):
+        import keras.backend as K
+        return K.mean(x, keepdims=(keepdims == 1), axis=axis)
+
+    lambda_layer = keras.layers.Lambda(target_layer, name=node_name)
+    layers[node_name] = lambda_layer(input_0)
+    layers[node_name].set_shape(layers[node_name]._keras_shape)
