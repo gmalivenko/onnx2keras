4ffa9b6c75ecc8f8765081b31c8185d0ded28951
Grigory Malivenko <nerox8664@gmail.com>
2019-11-08 14:33:17 +0200

    Update version and requirements.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open('README.md') as f:
 
 
 setup(name='onnx2keras',
-      version='0.0.13',
+      version='0.0.14',
       description='The deep learning models convertor',
       long_description=long_description,
       long_description_content_type='text/markdown',
