4ece13b6c6c84c17086f485f1ac1c6345d0f32d5
Jiayi (Jason) Liu <jason.liu@lge.com>
2020-01-14 10:43:54 -0800

    add conv3d

--- onnx2keras/convolution_layers.py
+++ onnx2keras/convolution_layers.py
@@ -34,11 +34,91 @@ def convert_conv(node, params, layers, node_name, keras_name):
     input_0 = ensure_tf_type(layers[node.input[0]], name="%s_const" % keras_name)
     n_groups = params['group'] if 'group' in params else 1
     dilation = params['dilations'][0] if 'dilations' in params else 1
-    pads = params['pads'] if 'pads' in params else [0, 0]
-    strides = params['strides'] if 'strides' in params else [1, 1]
+    pads = params['pads'] if 'pads' in params else [0, 0, 0]
+    strides = params['strides'] if 'strides' in params else [1, 1, 1]
 
     if len(W.shape) == 5:  # 3D conv
-        raise NotImplementedError('Not implemented')
+        logger.debug('3D convolution')
+        if pads[0] > 0 or pads[1] > 0 or pads[2] > 0:
+            logger.debug('Paddings exist, add ZeroPadding layer')
+            padding_name = keras_name + '_pad'
+            padding_layer = keras.layers.ZeroPadding3D(
+                padding=(pads[0], pads[1], pads[2]),
+                name=padding_name
+            )
+            layers[padding_name] = input_0 = padding_layer(input_0)
+        out_channels, channels_per_group, dimension, height, width = W.shape
+        W = W.transpose(2, 3, 4, 1, 0) 
+        in_channels = channels_per_group * n_groups
+
+        if n_groups != 1:
+            raise NotImplementedError("Not Implemented")
+            """
+        if n_groups == in_channels and n_groups != 1:
+            logger.debug('Number of groups is equal to input channels, use DepthWise convolution')
+            
+            W = W.transpose(0, 1, 3, 2, 4)
+            if has_bias:
+                weights = [W, bias]
+            else:
+                weights = [W]
+
+            conv = keras.layers.DepthwiseConv3D(
+                kernel_size=(dimension, height, width),
+                strides=(strides[0], strides[1], strides[2]),
+                padding='valid',
+                use_bias=has_bias,
+                activation=None,
+                depth_multiplier=1,
+                weights=weights,
+                dilation_rate=dilation,
+                bias_initializer='zeros', kernel_initializer='zeros',
+                name=keras_name
+            )
+            layers[node_name] = conv(input_0)
+        elif n_groups != 1:
+            logger.debug('Number of groups more than 1, but less than number of in_channel, use group convolution')
+
+            # Example from https://kratzert.github.io/2017/02/24/finetuning-alexnet-with-tensorflow.html
+            def target_layer(x, groups=n_groups, stride_y=strides[0], stride_x=strides[1]):
+                import tensorflow as tf
+                x = tf.transpose(x, [0, 2, 3, 1])
+
+                def convolve_lambda(i, k):
+                    import tensorflow as tf
+                    return tf.nn.conv2d(i, k, strides=[1, stride_y, stride_x, 1], padding='VALID')
+
+                input_groups = tf.split(axis=3, num_or_size_splits=groups, value=x)
+                weight_groups = tf.split(axis=3, num_or_size_splits=groups, value=W.transpose(0, 1, 2, 3))
+                output_groups = [convolve_lambda(i, k) for i, k in zip(input_groups, weight_groups)]
+
+                layer = tf.concat(axis=3, values=output_groups)
+
+                layer = tf.transpose(layer, [0, 3, 1, 2])
+                return layer
+
+            lambda_layer = keras.layers.Lambda(target_layer)
+            layers[node_name] = lambda_layer(input_0)
+            """
+        else:
+            if has_bias:
+                weights = [W, bias]
+            else:
+                weights = [W]
+
+            conv = keras.layers.Conv3D(
+                filters=out_channels,
+                kernel_size=(dimension, height, width),
+                strides=(strides[0], strides[1], strides[2]),
+                padding='valid',
+                weights=weights,
+                use_bias=has_bias,
+                activation=None,
+                dilation_rate=dilation,
+                bias_initializer='zeros', kernel_initializer='zeros',
+                name=keras_name
+            )
+            layers[node_name] = conv(input_0)
 
     elif len(W.shape) == 4:  # 2D conv
         logger.debug('2D convolution')
