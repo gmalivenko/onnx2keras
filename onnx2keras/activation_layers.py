Fix Softmax (use axis 1 and tf layer). Fix multiple outputs from dropout.

onnx2keras/activation_layers.py
@@ -140,8 +140,14 @@ def convert_softmax(node, params, layers, lambda_func, node_name, keras_name):
 
     input_0 = ensure_tf_type(layers[node.input[0]], name="%s_const" % keras_name)
 
-    softmax = keras.layers.Activation('softmax', name=keras_name)
-    layers[node_name] = softmax(input_0)
+    def target_layer(x):
+        import tensorflow as tf
+        return tf.nn.softmax(x, axis=1)
+
+    lambda_layer = keras.layers.Lambda(target_layer, name=keras_name)
+    layers[node_name] = lambda_layer(input_0)
+    layers[node_name].set_shape(layers[node_name].shape)
+    lambda_func[keras_name] = target_layer
 
 
 def convert_prelu(node, params, layers, lambda_func, node_name, keras_name):