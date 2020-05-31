0585e070ea96cad9f08eed3dcc7d429fe9b82dd0
Grigory Malivenko <nerox8664@gmail.com>
2020-05-31 22:35:27 +0300

    Update PyPi version.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open('README.md') as f:
 
 
 setup(name='onnx2keras',
-      version='0.0.20',
+      version='0.0.21',
       description='The deep learning models converter',
       long_description=long_description,
       long_description_content_type='text/markdown',
