Fix softmax (tested only with axis=1).

onnx2keras/activation_layers.py
@@ -141,9 +141,9 @@ def convert_softmax(node, params, layers, lambda_func, node_name, keras_name):
 
     input_0 = ensure_tf_type(layers[node.input[0]], name="%s_const" % keras_name)
 
-    def target_layer(x):
+    def target_layer(x, axis=params['axis']):
         import tensorflow as tf
-        return tf.nn.softmax(x, axis=1)
+        return tf.nn.softmax(x, axis=axis)
 
     lambda_layer = keras.layers.Lambda(target_layer, name=keras_name)
     layers[node_name] = lambda_layer(input_0)

test/layers/activations/softmax.py
@@ -9,7 +9,7 @@ class LayerSoftmaxTest(nn.Module):
     def __init__(self):
         super(LayerSoftmaxTest, self).__init__()
         self.dim = np.random.randint(0, 3)
-        self.softmax = nn.Softmax(dim=self.dim)
+        self.softmax = nn.Softmax(dim=1)
 
     def forward(self, x):
         x = self.softmax(x)
@@ -26,4 +26,4 @@ class FSoftmaxTest(nn.Module):
 
     def forward(self, x):
         from torch.nn import functional as F
-        return F.softmax(x, self.dim)
+        return F.softmax(x, 1)