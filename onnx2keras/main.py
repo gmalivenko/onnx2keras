f6dba12a997130f8c2cd58bce6f70b0fce4d3f0b
dorhar <doron.harnoy@tensorleap.ai>
2021-02-01 10:54:56 +0100

    add main and pycharm reformat

---
+++ onnx2keras/main.py
@@ -0,0 +1,16 @@
+import onnx
+from onnx2keras import onnx_to_keras
+
+
+def main():
+    onnx_file_path = "onnx_models/model.onnx"
+
+    # Load ONNX model
+    onnx_model = onnx.load(onnx_file_path)
+
+    # Call the converter (input - is the main model input name, can be different for your model)
+    k_model = onnx_to_keras(onnx_model, ['input_1'])
+
+
+if __name__ == '__main__':
+    main()
