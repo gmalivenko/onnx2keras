fabb03c98177686cbfc1072e87d78e53162c7136
dorhar <doron.harnoy@tensorleap.ai>
2022-05-31 13:50:21 +0300

    add all resnet models

--- test/models/test_resnet18.py
+++ test/models/test_resnet18.py
@@ -1,11 +1,12 @@
 import numpy as np
 import pytest
-from torchvision.models import resnet18, wide_resnet50_2
+from torchvision.models import resnet18, resnet34, resnet50, resnet101, resnet152, wide_resnet50_2, wide_resnet101_2
 
 from test.utils import convert_and_test
 
 
-@pytest.mark.parametrize('model_class', [resnet18, wide_resnet50_2])
+@pytest.mark.parametrize('model_class', [resnet18, resnet34, resnet50, resnet101, resnet152, wide_resnet50_2,
+                                         wide_resnet101_2])
 def test_resnet18(model_class):
     model = model_class()
     model.eval()
