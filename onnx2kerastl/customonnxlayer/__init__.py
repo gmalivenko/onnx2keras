e810afd3667e7269ffe0f1e62b23c2e95ca18082
doronHarnoy <62376065+doronHarnoy@users.noreply.github.com>
2022-07-31 16:40:27 +0300

    expose inner keras layer (#48)
    * exposing onnx layers
    
    * 0.0.46
    
    * 0.0.47

--- onnx2kerastl/customonnxlayer/__init__.py
+++ onnx2kerastl/customonnxlayer/__init__.py
@@ -1,3 +1,11 @@
 from onnx2kerastl.customonnxlayer.onnxhardsigmoid import OnnxHardSigmoid
+from onnx2kerastl.customonnxlayer.onnxsqrt import OnnxSqrt
+from onnx2kerastl.customonnxlayer.onnxreducemean import OnnxReduceMean
+from onnx2kerastl.customonnxlayer.onnxerf import OnnxErf
 
-onnx_custom_objects_map = {"OnnxHardSigmoid": OnnxHardSigmoid}
+onnx_custom_objects_map = {
+    "OnnxHardSigmoid": OnnxHardSigmoid,
+    "OnnxSqrt": OnnxSqrt,
+    "OnnxReduceMean": OnnxReduceMean,
+    "OnnxErf": OnnxErf
+}
