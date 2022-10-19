7ad5cddadaddd26297b5d52d85f1e5156cbbf615
dorhar <doron.harnoy@tensorleap.ai>
2022-10-19 09:44:14 +0300

    add abs

--- onnx2kerastl/customonnxlayer/__init__.py
+++ onnx2kerastl/customonnxlayer/__init__.py
@@ -2,10 +2,12 @@ from onnx2kerastl.customonnxlayer.onnxhardsigmoid import OnnxHardSigmoid
 from onnx2kerastl.customonnxlayer.onnxsqrt import OnnxSqrt
 from onnx2kerastl.customonnxlayer.onnxreducemean import OnnxReduceMean
 from onnx2kerastl.customonnxlayer.onnxerf import OnnxErf
+from onnx2kerastl.customonnxlayer.onnxabs import OnnxAbs
 
 onnx_custom_objects_map = {
     "OnnxHardSigmoid": OnnxHardSigmoid,
     "OnnxSqrt": OnnxSqrt,
     "OnnxReduceMean": OnnxReduceMean,
+    "OnnxAbs": OnnxAbs,
     "OnnxErf": OnnxErf
 }
