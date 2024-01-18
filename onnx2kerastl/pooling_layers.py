8482201ee1da92f5643c37f21cd25858c2793f78
tom Koren <tom.koren@tensorleap.ai>
2024-01-18 11:30:40 +0200

    cast k to int32 and support serialization for eager tensor in topk

--- onnx2kerastl/pooling_layers.py
+++ onnx2kerastl/pooling_layers.py
@@ -211,6 +211,8 @@ def convert_topk(node, params, layers, lambda_func, node_name, keras_name):
     to_sort = bool(params.get('sorted', 1))
     x = layers[node.input[0]]
     k = layers[node.input[1]][0]
+    if not is_numpy(k) and not K.is_keras_tensor(k): # Eager tensor does not serialize well
+        k = k.numpy().astype(np.int32)
     if not largest:
         in_tensor = -x
     else:
@@ -225,7 +227,12 @@ def convert_topk(node, params, layers, lambda_func, node_name, keras_name):
             ord_permute[axis] = rank - 1
             ord_permute[-1] = axis
             permuted = tf.transpose(in_tensor, ord_permute)
-        topk_res = tf.math.top_k(permuted, k=k, sorted=to_sort)
+        if (isinstance(k, tf.Tensor) or (not isinstance(k, (np.ndarray, np.generic)) and not isinstance(k, int) and tf.keras.backend.is_keras_tensor(k)))\
+                and k.dtype != tf.int32:  # otherwise top_k raise error
+            casted_k = tf.cast(k, tf.int32)
+        else:
+            casted_k = k
+        topk_res = tf.math.top_k(permuted, k=casted_k, sorted=to_sort)
         values_pre_permute = topk_res[0]
         indices_pre_permute = topk_res[1]
         topk_concat = tf.stack([values_pre_permute, tf.cast(indices_pre_permute, tf.float32)])
